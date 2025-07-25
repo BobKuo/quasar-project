5ff8b7e6ee1e18bf6294bd4ce7a8212ea9f37c40
bobkuo <geneteamgou@gmail.com>
2025-07-25 15:16:36 +0800

    修正swiper

--- src/boot/swiper.js
+++ src/boot/swiper.js
@@ -1,8 +1,8 @@
 // import function to register Swiper custom elements
-import { register } from 'swiper/element/bundle';
+import { register } from 'swiper/element/bundle'
 
 import { defineBoot } from '#q-app/wrappers'
 export default defineBoot(() => {
   // register Swiper custom elements
-  register();
+  register()
 })
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,82 +1,108 @@
 <template>
-  <!-- Swiper Container -->
-  <!--
-    slides-per-view="1"
-    space-between="10"
-    -->
-  <swiper-container
-    pagination-clickable="true"
-    navigation="true"
-    mousewheel-enabled="true"
-    free-mode="true"
-    speed="600"
+  <q-page class="flex flex-center" style="height: 1px">
+    <!-- Swiper Container -->
+    <!--
+
+
     parallax="true"
-  >
-    <div
-      slot="container-start"
-      class="parallax-bg"
-      style="background-image: url(https://swiperjs.com/demos/images/nature-1.jpg)"
-      data-swiper-parallax="-23%"
-    ></div>
-    <swiper-slide>
-      <div class="title" data-swiper-parallax="-300">Slide 1</div>
-      <div class="subtitle" data-swiper-parallax="-200">Subtitle</div>
-      <div class="text" data-swiper-parallax="-100">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum mattis velit, sit
-          amet faucibus felis iaculis nec. Nulla laoreet justo vitae porttitor porttitor.
-          Suspendisse in sem justo. Integer laoreet magna nec elit suscipit, ac laoreet nibh
-          euismod. Aliquam hendrerit lorem at elit facilisis rutrum. Ut at ullamcorper velit. Nulla
-          ligula nisi, imperdiet ut lacinia nec, tincidunt ut libero. Aenean feugiat non eros quis
-          feugiat.
-        </p>
-      </div>
-    </swiper-slide>
-    <swiper-slide>
-      <div class="title" data-swiper-parallax="-300">Slide 2</div>
-      <div class="subtitle" data-swiper-parallax="-200">Subtitle</div>
-      <div class="text" data-swiper-parallax="-100">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum mattis velit, sit
-          amet faucibus felis iaculis nec. Nulla laoreet justo vitae porttitor porttitor.
-          Suspendisse in sem justo. Integer laoreet magna nec elit suscipit, ac laoreet nibh
-          euismod. Aliquam hendrerit lorem at elit facilisis rutrum. Ut at ullamcorper velit. Nulla
-          ligula nisi, imperdiet ut lacinia nec, tincidunt ut libero. Aenean feugiat non eros quis
-          feugiat.
-        </p>
-      </div>
-    </swiper-slide>
-    <swiper-slide>
-      <div class="title" data-swiper-parallax="-300">Slide 3</div>
-      <div class="subtitle" data-swiper-parallax="-200">Subtitle</div>
-      <div class="text" data-swiper-parallax="-100">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum mattis velit, sit
-          amet faucibus felis iaculis nec. Nulla laoreet justo vitae porttitor porttitor.
-          Suspendisse in sem justo. Integer laoreet magna nec elit suscipit, ac laoreet nibh
-          euismod. Aliquam hendrerit lorem at elit facilisis rutrum. Ut at ullamcorper velit. Nulla
-          ligula nisi, imperdiet ut lacinia nec, tincidunt ut libero. Aenean feugiat non eros quis
-          feugiat.
-        </p>
-      </div>
-    </swiper-slide>
-    <!-- <swiper-slide><img src="https://picsum.photos/800/600/?random=10" /></swiper-slide>
+    -->
+    <swiper-container
+      ref="swiperRef"
+      slides-per-view="2"
+      space-between="10"
+      speed="600"
+      navigation="true"
+      pagination-clickable="true"
+      free-mode="true"
+      mousewheel-enabled="true"
+    >
+      <!-- https://swiperjs.com/demos/images/nature-1.jpg -->
+      <div
+        slot="container-start"
+        class="parallax-bg"
+        style="background-image: url(https://picsum.photos/1000/1000/?random=10)"
+        data-swiper-parallax="-23%"
+      ></div>
+      <swiper-slide>
+        <div class="title" data-swiper-parallax="-300">Slide 1</div>
+        <div class="subtitle" data-swiper-parallax="-200">Subtitle</div>
+        <div class="text" data-swiper-parallax="-100">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum mattis velit,
+            sit amet faucibus felis iaculis nec. Nulla laoreet justo vitae porttitor porttitor.
+            Suspendisse in sem justo. Integer laoreet magna nec elit suscipit, ac laoreet nibh
+            euismod. Aliquam hendrerit lorem at elit facilisis rutrum. Ut at ullamcorper velit.
+            Nulla ligula nisi, imperdiet ut lacinia nec, tincidunt ut libero. Aenean feugiat non
+            eros quis feugiat.
+          </p>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="title" data-swiper-parallax="-300">Slide 2</div>
+        <div class="subtitle" data-swiper-parallax="-200">Subtitle</div>
+        <div class="text" data-swiper-parallax="-100">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum mattis velit,
+            sit amet faucibus felis iaculis nec. Nulla laoreet justo vitae porttitor porttitor.
+            Suspendisse in sem justo. Integer laoreet magna nec elit suscipit, ac laoreet nibh
+            euismod. Aliquam hendrerit lorem at elit facilisis rutrum. Ut at ullamcorper velit.
+            Nulla ligula nisi, imperdiet ut lacinia nec, tincidunt ut libero. Aenean feugiat non
+            eros quis feugiat.
+          </p>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="title" data-swiper-parallax="-300">Slide 3</div>
+        <div class="subtitle" data-swiper-parallax="-200">Subtitle</div>
+        <div class="text" data-swiper-parallax="-100">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum mattis velit,
+            sit amet faucibus felis iaculis nec. Nulla laoreet justo vitae porttitor porttitor.
+            Suspendisse in sem justo. Integer laoreet magna nec elit suscipit, ac laoreet nibh
+            euismod. Aliquam hendrerit lorem at elit facilisis rutrum. Ut at ullamcorper velit.
+            Nulla ligula nisi, imperdiet ut lacinia nec, tincidunt ut libero. Aenean feugiat non
+            eros quis feugiat.
+          </p>
+        </div>
+      </swiper-slide>
+      <!-- <swiper-slide><img src="https://picsum.photos/800/600/?random=10" /></swiper-slide>
     <swiper-slide><img src="https://picsum.photos/400/300/?random=11" /></swiper-slide>
     <swiper-slide><img src="https://picsum.photos/800/600/?random=12" /></swiper-slide> -->
-  </swiper-container>
-  <!-- End Swiper Container -->
+    </swiper-container>
+    <!-- End Swiper Container -->
+  </q-page>
 </template>
 
 <script setup>
 //
+import { onMounted, ref } from 'vue'
+
+const swiperRef = ref(null)
+
+onMounted(() => {
+  if (swiperRef.value) {
+    const swiperParams = {
+      parallax: true,
+      // speed: 600,
+      // navigation: true,
+      // pagination: {
+      //   clickable: true,
+      // },
+      // mousewheel: true,
+    }
+
+    Object.assign(swiperRef.value, swiperParams)
+    swiperRef.value.initialize()
+  }
+})
 </script>
 
 <style scoped>
 swiper-container {
   width: 100%;
   height: 100%;
-  /* width: 100vw;
-  height: 100vh; */
+  background-color: #000;
+  border: 5px solid blue;
 
   /* --swiper-navigation-color: '#fff';
   --swiper-pagination-color: '#fff'; */
@@ -103,6 +129,8 @@ swiper-slide {
     max-width: 400px;
     line-height: 1.3;
   }
+
+  border: 1px solid green;
 }
 
 .parallax-bg {
@@ -114,5 +142,9 @@ swiper-slide {
   -webkit-background-size: cover;
   background-size: cover;
   background-position: center;
+}
+
+.q-page {
+  border: 1px solid red;
 }
 </style>
